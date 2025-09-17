22ef18754f885534103709edada3e7c75625e83b
Héctor Sharpe <cobranzaarriendos@gmail.com>
2025-09-17 13:12:11 -0300

    main

--- gastosmart-frontend/src/main.jsx
+++ gastosmart-frontend/src/main.jsx
@@ -1,50 +1,10 @@
 // src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
-import Login from "./pages/Login.jsx";
-import Register from "./pages/Register.jsx";
-import Dashboard from "./pages/Dashboard.jsx";
-import Categorias from "./pages/Categorias.jsx";
-import Gastos from "./pages/Gastos.jsx"; // 👈 import de la nueva página
-import ProtectedRoute from "./components/ProtectedRoute.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/categorias"
-          element={
-            <ProtectedRoute>
-              <Categorias />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/gastos"
-          element={
-            <ProtectedRoute>
-              <Gastos />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
