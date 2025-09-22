241268954b3282df254e548ba1c5acfd24614dd0
Héctor Sharpe <cobranzaarriendos@gmail.com>
2025-09-22 18:14:56 -0300

    mejoras visuales

--- gastosmart-frontend/src/pages/LandingPage.jsx
+++ gastosmart-frontend/src/pages/LandingPage.jsx
@@ -27,9 +27,67 @@ function LandingPage() {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
+        position: "relative",
+        overflow: "hidden",
         background: "linear-gradient(135deg, #90caf9 0%, #e3f2fd 100%)",
       }}
     >
+      {/* Fondo decorativo con ondas */}
+      <Box
+        component="div"
+        sx={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          backgroundImage: `url("data:image/svg+xml;utf8,
+            <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1440 320'>
+              <path fill='%23ffffff22' fill-opacity='1' d='M0,192L60,181.3C120,171,240,149,360,154.7C480,160,600,192,720,192C840,192,960,160,1080,149.3C1200,139,1320,149,1380,154.7L1440,160L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z'></path>
+            </svg>")`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "bottom",
+          opacity: 0.3,
+        }}
+      />
+
+      {/* Íconos decorativos financieros */}
+      <Typography
+        sx={{
+          position: "absolute",
+          top: "15%",
+          left: "10%",
+          fontSize: "3rem",
+          opacity: 0.15,
+        }}
+      >
+        💳
+      </Typography>
+      <Typography
+        sx={{
+          position: "absolute",
+          bottom: "20%",
+          right: "15%",
+          fontSize: "3.5rem",
+          opacity: 0.15,
+        }}
+      >
+        📊
+      </Typography>
+      <Typography
+        sx={{
+          position: "absolute",
+          bottom: "10%",
+          left: "20%",
+          fontSize: "3rem",
+          opacity: 0.15,
+        }}
+      >
+        💵
+      </Typography>
+
+      {/* Tarjeta central */}
       <Card
         sx={{
           maxWidth: 450,
@@ -38,6 +96,7 @@ function LandingPage() {
           borderRadius: 4,
           textAlign: "center",
           p: 3,
+          zIndex: 2, // 👈 para que quede sobre el fondo
         }}
       >
         <CardContent>
